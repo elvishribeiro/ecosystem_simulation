18aa6fde904370727103dd0ca579f3e82f7c63a9
Elvis H Ribeiro <elvishribeiro@gmail.com>
2019-12-10 01:45:58 -0300

    neighbour function is generic now

--- rules.c
+++ rules.c
@@ -2,39 +2,39 @@
 #include <time.h>
 
 /*find an empty slot*/
-coord_t empty_neighbour(object_t **eco, config_t conf, coord_t p) {
+coord_t neighbour(object_t **eco, config_t conf, coord_t p, int type) {
 	srand(time(NULL));
 	coord_t possible[4];
 	int possible_n = 0;
 
 	/*up*/
 	if (p.x - 1 >= 0 && 
-		eco[p.x - 1][p.y].type == EMPTY) {
+		eco[p.x - 1][p.y].type == type) {
 		possible[possible_n++] = (coord_t){.x = p.x - 1, .y = p.y};
 
 	}
 
 	/*right*/
 	if (p.y + 1 < conf.C && 
-		eco[p.x][p.y + 1].type == EMPTY) {
+		eco[p.x][p.y + 1].type == type) {
 		possible[possible_n++] = (coord_t){.x = p.x, .y = p.y + 1};
 	}
 
 	/*down*/
 	if (p.x + 1 < conf.L && 
-		eco[p.x + 1][p.y].type == EMPTY) {
+		eco[p.x + 1][p.y].type == type) {
 		possible[possible_n++] = (coord_t){.x = p.x + 1, .y = p.y};
 	}
 
 	/*left*/
 	if (p.y - 1 >= 0 && 
-		eco[p.x][p.y - 1].type == EMPTY) {
+		eco[p.x][p.y - 1].type == type) {
 		possible[possible_n++] = (coord_t){.x = p.x, .y = p.y - 1};
 	}
 
 	if (possible_n > 0)
 		if (possible_n > 1)
-			return possible[(conf.GEN + p.x +p.y) % possible_n];
+			return possible[(conf.GEN + p.x +p.y) % possible_n];		/*choose one of the neighbours*/
 		else
 			return possible[0];
 	else 
@@ -56,7 +56,7 @@ void rabbit_insert (object_t **eco, object_t **next_eco, rabbit_t r, coord_t p) 
 
 /* Make a move if a available slot is found and returns the new position*/
 coord_t rabbit_move (object_t **eco, object_t **next_eco, config_t conf, coord_t p) {
-	coord_t new = empty_neighbour(eco, conf, p);										
+	coord_t new = neighbour(eco, conf, p, EMPTY);										
 	if (new.x >= 0 && new.y >= 0){
 		if (RABBIT_P(next_eco[p.x][p.y].entity)->age >= conf.GEN_PROC_COELHOS) {		/* Reproduction */
 			rabbit_insert(eco, next_eco, (rabbit_t){.age = 0}, new);
