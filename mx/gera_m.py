71d803902ade934fea5453df3ba09db09248e6a9
jardelcarvalho <jardelfelipe.carvalho@gmail.com>
2019-12-12 11:14:20 -0300

    fix

--- mx/gera_m.py
+++ mx/gera_m.py
@@ -13,14 +13,15 @@ for i in range(10):
     with open('m' + str(i) + '.txt', 'w') as f:
         f.write(str(G_P_C) + ' ' + str(G_P_R) + ' ' + str(G_C_R) + ' ' + str(N_GEN) + ' ' + str(L) + ' ' + str(L) + ' ' + str(N) + '\n')
         pos = []
-        for _ in range(N):
-            x = random.randint(0, L)
-            y = random.randint(0, L)
+        for _ in range(N - 1):
             while True:
                 x = random.randint(0, L)
                 y = random.randint(0, L)
                 if (x, y) not in pos:
-                    pos += [(x, y)] 
+                    pos += [(x, y)]
                     break
             b = ['ROCHA', 'RAPOSA', 'COELHO'][random.randint(0, 2)]
             f.write(str(b) + ' ' + str(x) + ' ' + str(y) + '\n')
+        G_P_C += 1
+        G_P_R += 1
+        G_C_R += 1
