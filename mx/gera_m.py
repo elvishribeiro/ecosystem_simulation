dcac5c8eb4cd2d1055bd8dfeb53150611ef35ae3
jardelcarvalho <jardelfelipe.carvalho@gmail.com>
2019-12-12 14:56:35 -0300

    new matrix gen

--- mx/gera_m.py
+++ mx/gera_m.py
@@ -1,27 +1,20 @@
-import random
-
-L = random.randint(100, 1000)
-L = 6
-N = random.randint(int(L * L * .35), int(L * L * .5))
-G_P_C = random.randint(0, 5)
-G_P_R = random.randint(0, 5)
-G_C_R = random.randint(2, 8) 
-
 N_GEN = 30
 
-for i in range(10):
-    with open('n6/' + str(i) + '.txt', 'w') as f:
-        f.write(str(G_P_C) + ' ' + str(G_P_R) + ' ' + str(G_C_R) + ' ' + str(N_GEN) + ' ' + str(L) + ' ' + str(L) + ' ' + str(N) + '\n')
-        pos = []
-        for _ in range(N - 1):
-            while True:
-                x = random.randint(0, L-1)
-                y = random.randint(0, L-1)
-                if (x, y) not in pos:
-                    pos += [(x, y)]
-                    break
-            b = ['ROCHA', 'RAPOSA', 'COELHO'][random.randint(0, 2)]
-            f.write(str(b) + ' ' + str(x) + ' ' + str(y) + '\n')
+x_pos = [i for i in range(L - 1)]
+y_pos = [i for i in range(L - 1)]
+random.shuffle(x_pos)
+random.shuffle(y_pos)
+
+for i in range(1):
+    with open('m' + str(i) + '.txt', 'w') as f:
+        f.write(str(G_P_C) + ' ' + str(G_P_R) + ' ' + str(G_C_R) + ' ' + str(N_$
+        for j in tqdm(range(N - 1), position=0):
+            if j < L - 1:
+                x = x_pos[j]
+                y = y_pos[j]
+                b = ['ROCHA', 'RAPOSA', 'COELHO'][random.randint(0, 2)]
+                f.write(str(b) + ' ' + str(x) + ' ' + str(y) + '\n')
         G_P_C += 1
         G_P_R += 1
         G_C_R += 1
+
