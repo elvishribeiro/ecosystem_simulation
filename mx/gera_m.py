61e2adf574450c821556aba62120a0175ce18342
jardelcarvalho <jardelfelipe.carvalho@gmail.com>
2019-12-13 00:20:07 -0300

    new matrix generator

--- mx/gera_m.py
+++ mx/gera_m.py
@@ -2,31 +2,80 @@ import random
 from tqdm import tqdm
 import math 
 
+def gera_posicoes(a=[]):
+    if a == []:
+        return None
+    tuplas = []
+    k = len(a) - 1
+    x = [-1 for _ in a]
+    i = 0
+    n = 0
+    while i != -1:
+        if x[i] != a[i]:
+            x[i] += 1
+            if i < k:
+                i += 1
+            else:
+                tuplas += [(x[0], x[1])]
+                n += 1
+                pass
+        else:
+            x[i] = -1
+            i -= 1
+    return tuplas
 
-L = [int(math.sqrt(300000)), int(math.sqrt(600000)), int(math.sqrt(1200000)), int(math.sqrt(2400000)), int(math.sqrt(4800000))]
-G_P_C = random.randint(1, 5)
-G_P_R = random.randint(1, 5)
-G_C_R = random.randint(2, 8)
+#número de entradas da matriz
+L = [
+    int(math.sqrt(300000)), 
+    int(math.sqrt(600000)), 
+    int(math.sqrt(1200000)), 
+    int(math.sqrt(2400000)), 
+    int(math.sqrt(4800000))
+    ]
 
-N_GEN = 30
+#É SÓ ALTERAR O NÚMERO DE GERAÇÕES, OS OUTROS PARÂMETROS SÃO ESTIMADOS AUTOMATICAMENTE
+#NÚMERO DE GERAÇÕES DEVE SER MAIOR OU IGUAL À 24 PRAS EQUAÇÕES FUNCIONAREM
+N_GEN = 365
 
-for i in range(len(L)):
-    x_pos = [i for i in range(L[i] - 1)]
-    y_pos = [i for i in range(L[i] - 1)]
-    random.shuffle(x_pos)
-    random.shuffle(y_pos)
+#estimação gerações
+GEN_PROC_COELHO = int((1 / 12) * N_GEN)
+GEN_PROC_RAPOSA = int((50 / (12 * 30)) * N_GEN)
+GEN_COMIDA_RAPOSA = int((15 / (12 * 30)) * N_GEN)
 
-    N = random.randint(int(L[i] * L[i] * .35), int(L[i] * L[i] * .5))
-    with open('m' + str(i) + '.txt', 'w') as f:
-        f.write(str(G_P_C) + ' ' + str(G_P_R) + ' ' + str(G_C_R) + ' ' + str(N_GEN) + ' ' + str(L[i]) + ' ' + str(L[i]) + ' ' + str(N) + '\n')
-        #f.write(str(G_P_C) + ' ' + str(G_P_R) + ' ' + str(G_C_R) + ' ' + str(N))
-        for j in tqdm(range(N - 1), position=0):
-            if j < L[i] - 1:
-                x = x_pos[j]
-                y = y_pos[j]
-                b = ['ROCHA', 'RAPOSA', 'COELHO'][random.randint(0, 2)]
-                f.write(str(b) + ' ' + str(x) + ' ' + str(y) + '\n')
-        G_P_C += 1
-        G_P_R += 1
-        G_C_R += 1
+#porcentagem de cada objeto no mapa
+porc_coelhos = .5
+porc_raposas = .3
+porc_rochas = .2
 
+for L_i in L:
+    pos = gera_posicoes([L_i - 1, L_i - 1])
+    random.shuffle(pos)
+
+    N = int(((L_i - 1) ** 2) * .4)
+    n_obj = [[int(N * porc_coelhos), 'COELHO'], [int(N * porc_raposas), 'RAPOSA'], [int(N * porc_rochas), 'ROCHA']]
+
+    total = sum([o[0] for o in n_obj])
+    if total < N:
+        #aumenta o número de rochas
+        n_obj[-1][0] += N - total
+        total += N - total
+
+    with open('m' + str(L_i - 1) + '.txt', 'w') as f:
+        f.write(
+            str(GEN_PROC_COELHO) + ' ' + 
+            str(GEN_PROC_RAPOSA) + ' ' + 
+            str(GEN_COMIDA_RAPOSA) + ' ' + 
+            str(N_GEN) + ' ' + 
+            str(L_i - 1) + ' ' + 
+            str(L_i - 1) + ' ' + 
+            str(total) + '\n'
+            )
+
+        k = 0
+        for j in tqdm(range(total), position=0):
+            x, y = pos[j]
+            b = n_obj[k][1]
+            f.write(str(b) + ' ' + str(x) + ' ' + str(y) + '\n')
+            n_obj[k][0] -= 1
+            if n_obj[k][0] == 0:
+                k += 1
